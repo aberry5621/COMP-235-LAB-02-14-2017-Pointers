b7c3824f6ea295d5726f231379c0179258c2d655
aberry5621 <aberry5621@gmail.com>
2017-03-19 17:04:31 -0700

    Initial Commit

---
+++ COMP 235 LAB 02 14 2017 Pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP 235 LAB 02 14 2017 Pointers
+//
+//  Created by ax on 3/19/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
